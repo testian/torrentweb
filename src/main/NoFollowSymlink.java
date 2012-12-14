5dbf6324d6fb4d93dac50e4dd916f3892fddae1f
Dimitri Nüscheler <testi@testi-acer.(none)>
2012-12-14 09:30:08 +0100

    Reliable check

--- src/main/NoFollowSymlink.java
+++ src/main/NoFollowSymlink.java
@@ -431,14 +431,15 @@ public class NoFollowSymlink extends SecurityManager {
 	public static boolean isSymlink(File file) throws IOException {
 		  if (file == null)
 		    throw new NullPointerException("File must not be null");
-		  File canon;
+		  return !file.getCanonicalPath().equals(file.getAbsolutePath());
+		  /*File canon;
 		  if (file.getParent() == null) {
 		    canon = file;
 		  } else {
 		    File canonDir = file.getParentFile().getCanonicalFile();
 		    canon = new File(canonDir, file.getName());
 		  }
-		  return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+		  return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());*/
 		}
 	
 	
